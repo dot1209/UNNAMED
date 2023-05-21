0669b2f00a50bf1c782708ac6ad66d5d1222f546
dot <dzwu98@gmail.com>
2023-05-22 00:34:53 +0800

    ✨ Use MUI backdrop to display reasoning path

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Button from "@mui/material/Button";
 
-function MyButton(props) {
-  const { label, onClick, margin} = props;
-  console.log(props);
+function MyButton({ label, onClick, margin }) {
   return (
     <Button variant="contained" onClick={onClick} style={{
       margin: `${margin}px`
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import MyButton from "./Button";
 import { NeoGraph } from "./KnowledgeGraph";  
+import { Backdrop } from "@mui/material";
+import { Grid } from "@mui/joy";
 
 const Message = ({ message, index }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   if (message.cypher === "") {
     return (
     <div className={`message ${message.source}`}>
@@ -11,17 +19,31 @@ const Message = ({ message, index }) => {
   else {
     return (
       <div className={`message ${message.source}`}>
-        <NeoGraph
-          width={400}
-          height={300}
-          containerId={`message-${index}`}
-          neo4jUri={process.env.REACT_APP_NEO4J_URI}
-          neo4jUser={process.env.REACT_APP_NEO4J_USER}
-          neo4jPassword={process.env.REACT_APP_NEO4J_PASSWORD}
-          cypher={message.cypher}
-          backgroundColor={"#f5f5f5"}
+        <MyButton
+          label={"Show the reasoning path"}
+          onClick={handleOpen}
+          margin={8}
         />
-      </div>
+          <Backdrop open={open}>
+            <Grid justifyContent={"center"} alignItems={"center"}>
+              <NeoGraph
+                width={800}
+                height={600}
+                containerId={`message-${index}`}
+                neo4jUri={process.env.REACT_APP_NEO4J_URI}
+                neo4jUser={process.env.REACT_APP_NEO4J_USER}
+                neo4jPassword={process.env.REACT_APP_NEO4J_PASSWORD}
+                cypher={message.cypher}
+                backgroundColor={"#f5f5f5"}
+              />
+              <MyButton
+                label={"Close the reasoning path"}
+                onClick={handleClose}
+                margin={8}
+              />
+            </Grid>
+          </Backdrop>
+        </div>
     )
   }
 }
